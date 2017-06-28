36a5d9e61f2f20f0530a73e1afcabed4d027d061
Vikos <vikos@vikos.me>
2017-06-28 07:17:29 +0200

    Bakcup (unstable)

--- configuration.js
+++ configuration.js
@@ -1,62 +1,92 @@
-function buildOptions() {
-    return {
-        _domains: {},
-        _buildDomain: function(name, filter, templates) {
-            var domain = {
-                _name: undefined,
-                _filter: undefined,
-                _templates: [],
-                name: function(name) {
-                    if (name === undefined) {
-                        return this._name;
-                    } else {
-                        this._name = name;
-                        return this
-                    }
-                },
-                filter: function (filter) {
-                    if (filter === undefined) {
-                        return this._filter;
-                    } else {
+function buildOptions(domains) {
 
-                    }
-                },
-                template: function (key,template) {
-                    if (key === undefined) {
-                        throw new Error("Template key must be defined!");
-                    } else if (template === undefined) {
-                        return this._templates[key];
-                    } else {
-                        this._templates[key] = template
-                        return this;
-                    }
-                },
-                templates: function (templates) {
-                    if (templates === undefined) {
-                        return this._templates;
-                    } else if (typeof templates === 'object') {
-                       for (i in templates) {
-                           this._templates = templates[i];
-                       }
-                    } else {
-                        throw new Error("Invalid parameter. Teamplates must be object[Key>value]. Given'" + typeof templates + "'");
-                    }
+    var options = {_domains: {}};
+    options.domains = function(domains) {
+            if (domains === undefined) {
+                return this._templates;
+            } else if (typeof domains === 'object') {
+
+                this._domains = [];
+
+                for (i in domains) {
+                    this.domain(i, domains[i]);
                 }
-            };
+            } else {
+                throw new Error("Invalid parameter. Teamplates must be object[Key>value]. Given'" + typeof templates + "'");
+            }
+        };
 
-            return domain.name(name).filter(filter).templates(templates);
-        },
-        newDomain: function() {
-            return
-        },
-        getDomain(key) {
-            return _
-        }
-    };
+    options.domain = function(index, domain) {
+            if (domain === undefined) {
+                this._domains[index];
+                return this;
+            } else {
+                return this._domains[index];
+            }
+        };
+
+    if (typeof domains !== undefined) {
+        options.domains(domains)
+    }
+
+    return options
 }
 
-(function () {
-    document._gatlingDomains = {};
+function buildDomain(name, filter, templates) {
+    var domain = {
+        _name: undefined,
+        _filter: undefined,
+        _templates: []
+    };
 
-})();
+    domain.name = function(name) {
+        if (name === undefined) {
+            return this._name;
+        } else {
+            this._name = name;
+            return this
+        }
+    };
+
+    domain.filter = function (filter) {
+        if (filter === undefined) {
+            return this._filter;
+        } else {
+            this._filter = filter;
+            return this
+        }
+    };
+
+    domain.template = function (key,template) {
+        if (key === undefined) {
+            throw new Error("Template key must be defined!");
+        } else if (template === undefined) {
+            return this._templates[key];
+        } else {
+            this._templates[key] = template
+            return this;
+        }
+    };
+
+    domain.templates = function (templates) {
+        if (templates === undefined) {
+            return this._templates;
+        } else if (typeof templates === 'object') {
+
+            this._templates = {};
+
+            for (key in templates) {
+                this.templates(key, templates.key);
+            }
+        } else {
+            throw new Error("Invalid parameter. Teamplates must be object[Key>value]. Given'" + typeof templates + "'");
+        }
+    };
+
+    domain.name(name);
+    domain.filter(filter);
+    domain.templates(templates);
+
+    return domain
+}
 
--- options.js
+++ options.js
@@ -1,50 +1,58 @@
+var DOMAIN_PROPERTY = "_domain";
+
 $(document).ready(function (e) {
-   initTemplateStorage()
-   registerListeners();
+    var options = buildOptions();
+
+    $("#new-domain").on("click", function() {
+        addDomain(buildDomain("New Domain"));
+    });
+
+    $("#save").on("click", function() {
+        // @todo: collect domains
+        saveOptions(options)
+    });
+
+    $("#domains").on("change", function() {
+        var selector = $(this).find(":selected");
+
+        renderDomain(selector.prop(DOMAIN_PROPERTY), function (name) {
+            selector.text(name);
+        });
+    });
 });
 
-function initTemplateStorage() {
-    var domainPrototype = {
-        key: "",
-        filter: "",
-        templates: []
-    };
-
-    var temp
-
-    document._gatlingOptions = {};
-    document.getDomain = function(key) {
-        if document._gatlingOptions
-    };
-}
-
-function registerListeners() {
-    $("#new-domain").on("click", addNewDomain);
-}
-
-function addNewDomain() {
-    newDomain({name: "New Domain"})
-}
-
-function renderDomain(domain) {
-    // Add Selector
+function addDomain(domain) {
     var selector = $("<option>");
-    selector.text(domain.name);
+    selector.text(domain.name());
+    selector.prop(DOMAIN_PROPERTY, domain);
     $("#domains").append(selector);
+
+    renderDomain(domain, function (name) {
+        selector.text(name);
+    });
+}
+
+
+function renderDomain(domain, onChangeOfName) {
+    console.log("render Domain");
 
     // Name input
     var name = $("<input type='text' style='display: block;' name='name' />");
-    name.val(domain.name);
+    name.val(domain.name());
 
     name.on("keyup", function (e) {
-       tabHead.text(name.val());
+        onChangeOfName(name.val());
+        domain.name(name.val());
     });
     var nameLabel = $("<label>").text("Name");
 
     // Filter (domain regexp input)
     var filter = $("<input type='text' style='display: block;' name='filter' />");
-    filter.val(domain.filter);
+    filter.val(domain.filter());
     filter.attr("title", "JS Regular expression which matching to the domain");
+    filter.on("change", function (e) {
+        domain.filter(filter.val());
+    });
 
     var filterLabel = $("<label>").text("Filter");
 
@@ -55,25 +63,31 @@ function renderDomain(domain) {
     options.append(filterLabel);
     options.append(filter);
 
+    $("#domain-options").empty();
     $("#domain-options").append(options);
+    $("#domain-options").flash();
 
-    renderTemplates(domain.templates);
+    for (key in domain.templates()) {
+        renderTemplate(key, domain.template(key), domain);
+    }
 }
 
-function renderTemplateOptions() {
-
-}
-
-function renderTemplate(template) {
+function renderTemplate(key, template) {
     var container = document.createElement('div');
 
     var idInput = document.createElement("input");
-    idInput.vale = id;
+    idInput.vale = key;
     idInput.required = true;
+    idInput.addEventListener("change", function() {
+    });
 
     var templateInput = document.createElement("textarea");
     templateInput.value = template;
     templateInput.required = true;
+
+    // @todo: Add oncahnge event listener
+    templateInput.addEventListener("change", function() {
+    });
 
     container.appendChild(idInput);
     container.appendChild(templateInput);
@@ -83,3 +97,6 @@ function renderTemplate(template) {
     return container
 }
 
+function saveOptions(options) {
+    console.log("Saving options", options);
+}
